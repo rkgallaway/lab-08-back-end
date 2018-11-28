d86b78057dc4e78e38337860a5d2514a28c081cf
rkgallaway <rkgallaway@msn.com>
2018-11-28 00:11:03 -0800

    all APIs caching/SQL

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE locations, weathers;
+DROP TABLE locations, weathers, yelps, movies, meetups, trails;
 
 CREATE TABLE IF NOT EXISTS locations ( 
     id SERIAL PRIMARY KEY,
@@ -26,3 +26,48 @@ CREATE TABLE IF NOT EXISTS weathers (
       created_at BIGINT,
       location_id INTEGER NOT NULL REFERENCES locations(id)
   );
+
+  CREATE TABLE IF NOT EXISTS movies (
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(255),
+      released_on VARCHAR(255),
+      average_votes VARCHAR(255),
+      total_votes VARCHAR(255),
+      image_url VARCHAR(255),
+      overview VARCHAR(750),
+      popularity VARCHAR(255),
+      created_at BIGINT,
+      location_id INTEGER NOT NULL REFERENCES locations(id)
+  );
+
+    CREATE TABLE IF NOT EXISTS meetups (
+      id SERIAL PRIMARY KEY,
+      link VARCHAR(255),
+      name VARCHAR(255),
+      host VARCHAR(255),
+      creation_date VARCHAR(255),
+      created_at BIGINT,
+      location_id INTEGER NOT NULL REFERENCES locations(id)
+  );
+
+      CREATE TABLE IF NOT EXISTS trails (
+      id SERIAL PRIMARY KEY,
+      trail_url VARCHAR(255),
+      name VARCHAR(255),
+      location VARCHAR(255),
+      length VARCHAR(255),
+      condition_date VARCHAR(255),
+      condition_time VARCHAR(255),
+      conditions VARCHAR(255),
+      stars VARCHAR(255),
+      star_votes VARCHAR(255),
+      summary VARCHAR(255),
+      created_at BIGINT,
+      location_id INTEGER NOT NULL REFERENCES locations(id)
+  );
+
+
+  
+
+
+      
